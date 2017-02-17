63b44e5c283f1f04f85f96fb61ca1a86058b240a
slanto <slawek.antos@outlook.com>
2017-02-17 21:32:05 +0100

    Styling element using [class]

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,6 +4,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     selector: 'event-thumbnail',
     templateUrl: 'app/events/event-thumbnail.component.html',
     styles: [`
+        .green { color: #003300 !important; }
+        .bold { font-weight: bold; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
         .thumbnail { min-height: 210px; }
