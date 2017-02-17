67e4584fcade10e64720d994f48999fd77a7ea28
slanto <slawek.antos@outlook.com>
2017-02-17 14:41:49 +0100

    Adding event list component

---
+++ app/events/events-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'events-list',
+    templateUrl: 'app/events/events-list.component.html'
+})
+export class EventsListComponent {
+    event = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '9/26/2036',
+        time: '10:00am',
+        price: 599.99,
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1057 DD',
+            city: 'New York',
+            country: 'USA'
+        }  
+    };
+}
