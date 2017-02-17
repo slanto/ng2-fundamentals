63b83c009717c002e4616821d2b091ed0bbd1cb4
slanto <slawek.antos@outlook.com>
2017-02-17 15:07:30 +0100

    Adding event thumbnail component and communication with it using @Input

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core'
     templateUrl: 'app/events/events-list.component.html'
 })
 export class EventsListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '9/26/2036',
