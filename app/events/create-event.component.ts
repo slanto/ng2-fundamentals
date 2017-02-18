0752b439b4ed83fa8960cf3adb7d895045d561f5
slanto <slawek.antos@outlook.com>
2017-02-18 23:00:02 +0100

    Using ngModelGroup to group location according to model

--- app/events/create-event.component.ts
+++ app/events/create-event.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
 
+import { EventService } from './shared/index'
+
 @Component({
-    templateUrl: '/app/events/create-event.component.html'
+    templateUrl: '/app/events/create-event.component.html',
+    styles: [`
+    em { color: #E05C65; float: right; padding-left: 10px; }
+    .error input { background-color: #E3C3C5; }
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-placeholder { color: #999; }
+    .error :-moz-placeholder { color: #999; } 
+    .error ::ms-input-placeholder { color: #999; }
+  `]
 })
 export class CreateEventComponent {
     isDirty: boolean = true;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private eventService: EventService) {
 
     };
 
     cancel() {
+        this.router.navigate(['/events']);
+    };
+
+    saveEvent(formValues) {
+        this.eventService.saveEvent(formValues);
+        this.isDirty = false;
         this.router.navigate(['/events']);
     };
 }
